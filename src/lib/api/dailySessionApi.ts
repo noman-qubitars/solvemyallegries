cd053e183148c7c699076e43edb83113b37fc83c
Muhammad Noman <155643503+noman-qubitars@users.noreply.github.com>
2025-12-09 13:48:56 +0500

    Add ngrok-skip-browser-warning header

--- src/lib/api/dailySessionApi.ts
+++ src/lib/api/dailySessionApi.ts
@@ -15,6 +15,7 @@ const baseQuery = fetchBaseQuery({
       headers.set('authorization', `Bearer ${token}`);
     }
     headers.set('Content-Type', 'application/json');
+    headers.set('ngrok-skip-browser-warning', 'true');
     return headers;
   },
 });
